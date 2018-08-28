ebf7ee81b96b1b5cc98bb3027636dcb17a7005fa
Atanas Chuchulev <achuchulev@Atanass-MacBook-Pro.local>
2018-08-28 14:40:44 +0300

    update readme

--- app.py
+++ app.py
@@ -2,15 +2,6 @@
 
 # define count variable
 count = 0
+count += 1
 
-def main():
-#use the global variable instead of a local one
-	global count
-#increment count by 1
-	count+=1
-	return "{}".format(count)
-
-if __name__ == '__main__':
-	main()
-	print count
-#	print "{}".format(count) 
+print count
