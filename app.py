2389376b74cc22f54b8dce09ae54722744f0bf77
Atanas Chuchulev <achuchulev@Atanass-MacBook-Pro.local>
2018-08-28 14:08:23 +0300

    update readme and webapp

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 
 # define count variable
-count += 1
+count = 0
 
-print count
+def main():
+#use the global variable instead of a local one
+	global count
+#increment count by 1
+	count+=1
+	return "{}".format(count)
 
+if __name__ == '__main__':
+	main()
+	print count
+#	print "{}".format(count) 
